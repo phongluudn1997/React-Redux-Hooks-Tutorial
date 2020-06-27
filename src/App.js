0db6610c9a0ed17fdb29a1e85e84e46d0035b045
Huynh Phong Luu <luuhuynh@me.com>
2020-06-27 23:27:37 +0700

    start: burger app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+export default App;
