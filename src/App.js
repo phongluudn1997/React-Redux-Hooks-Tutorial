5121e412074c660b35ec578b843413d95c3e9aad
Phong Luu <luuhuynh@me.com>
2020-08-28 20:14:23 +0700

    cleaned

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
 import "./App.css";
-import Layout from "./components/Layout/Layout";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return (
-      <h1>
-        <Layout>
-          <p>Test Main Page.</p>
-        </Layout>
-      </h1>
-    );
+    return <h1>Hello World</h1>;
   }
 }
 
